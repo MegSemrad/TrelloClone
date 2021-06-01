8e3f0e8cd264478312d35aae715f78e8eca313bc
Meghan Semrad <mlsemrad753@gmail.com>
2021-06-01 12:31:57 -0500

    added methods to userRepository

--- TrelloClone/Repositories/UserRepository.cs
+++ TrelloClone/Repositories/UserRepository.cs
@@ -1,1 +1,149 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using TrelloClone.Models;
+using TrelloClone.Utils.cs;
+
+
+namespace TrelloClone.Repositories
+{
+    public class UserRepository : BaseRepository
+    {
+
+        public UserRepository(IConfiguration configuration) : base(configuration) { }
+
+
+        public List<User> GetAllUsers()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT u.Id, u.UserName, u.Email, u.FirebaseUserId
+                    FROM [User] u";
+
+                    User user = null;
+
+                    var reader = cmd.ExecuteReader();
+
+                    var users = new List<User>();
+
+                    while (reader.Read())
+                    {
+                        user = new User()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirebaseUserId = reader.GetString(reader.GetOrdinal("FirebaseUserId")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            UserName = reader.GetString(reader.GetOrdinal("UserName")),
+                        };
+
+                        users.Add(user);
+                    }
+                    reader.Close();
+                    return users;
+                }
+            }
+        }
+
+
+
+
+
+        public User GetUserById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT u.Id, u.UserName, u.Email, u.FirebaseUserId
+                    FROM [User] u
+                    WHERE u.Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    User user = null;
+
+                    var reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        user = new User()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseUserId"),
+                            UserName = DbUtils.GetString(reader, "UserName"),
+                            Email = DbUtils.GetString(reader, "Email")
+                        };
+                    }
+                    reader.Close();
+                    return user;
+                }
+            }
+        }
+
+
+
+
+
+        public User GetByFirebaseUserId(string FirebaseUserId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT u.Id, u.UserName, u.Email, u.FirebaseUserId
+                        FROM [User] u
+                        WHERE u.FirebaseUserId = @FirebaseUserId";
+
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", FirebaseUserId);
+
+                    User user = null;
+
+                    var reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        user = new User()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            FirebaseUserId = DbUtils.GetString(reader, "FirebaseUserId"),
+                            UserName = DbUtils.GetString(reader, "UserName"),
+                            Email = DbUtils.GetString(reader, "Email")
+                        };
+                    }
+                    reader.Close();
+                    return user;
+                }
+            }
+        }
+
+
+
+
+
+        public void Add(User user)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO [User] (FirebaseUserId, UserName, Email)
+                                        OUTPUT INSERTED.ID
+                                        VALUES (@FirebaseUserId, @UserName, @Email)";
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", user.FirebaseUserId);
+                    DbUtils.AddParameter(cmd, "@UserName", user.UserName);
+                    DbUtils.AddParameter(cmd, "@Email", user.Email);
+
+                    user.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
