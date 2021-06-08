eb662f21e3531c10f33dffc67878592499c3d632
Meghan Semrad <mlsemrad753@gmail.com>
2021-06-08 10:20:55 -0500

    updated index.js file to include the call to the firebase.initializeApp

--- client/trello-clone/src/index.js
+++ client/trello-clone/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from "firebase";
+import { BrowserRouter as Router } from "react-router-dom";
+import TrelloClone from './components/TrelloClone';
+import { UserProvider } from "./providers/UserProvider";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+};
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <UserProvider>
+        <TrelloClone />
+      </UserProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
