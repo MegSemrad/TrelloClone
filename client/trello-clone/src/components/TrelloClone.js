05935455ebe628503ba48644269716e29eca2f6a
Meghan Semrad <mlsemrad753@gmail.com>
2021-06-10 09:55:02 -0500

    added TrelloClone.js code

---
+++ client/trello-clone/src/components/TrelloClone.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from "react-router-dom";
+import Login from "./login.js";
+import Register from "./register.js";
+import NavBar from "../components/NavBar.js";
+import ApplicationViews from "./ApplicationViews";
+import { UserContext } from "../providers/UserProvider";
+
+
+
+function TrelloClone(props) {
+
+    const { isLoggedIn } = useContext(UserContext);
+
+    return (
+        <>
+            <Route path="/">
+                {isLoggedIn ?
+                    <>
+                        <NavBar />
+                        <ApplicationViews />
+                    </>
+                    : <Redirect to="/login" />
+                }
+            </Route>
+
+            <Route path="/login">
+                <Login />
+            </Route>
+
+            <Route path="/register">
+                <Register />
+            </Route>
+        </>
+    )
+};
+
+
+export default TrelloClone;
