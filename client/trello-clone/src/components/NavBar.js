f17c6dbe82a37b8f5982cf6e6002b0e7761969a8
Meghan Semrad <mlsemrad753@gmail.com>
2021-06-13 16:06:05 -0500

    added navbar.js code

---
+++ client/trello-clone/src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext } from 'react';
+import { NavLink as RRNavLink, Redirect } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    NavbarText
+} from 'reactstrap';
+import { UserContext } from "./providers/UserProvider.js"
+
+
+
+export default function NavBar() {
+    const { isLoggedIn, logout } = useContext(UserContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <NavbarText>Trello Clone</NavbarText>
+
+                    <Nav navbar>
+                        {isLoggedIn &&
+                            <>
+                                <NavItem>
+                                    <a aria-current="page" className="nav-link"
+                                        style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
+                                </NavItem>
+                            </>
+                        }
+                        {!isLoggedIn &&
+                            <Redirect to="/login" />
+                        }
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+};
